30f8a3b55bbb2fddc58882e4461575ac2d956c7e
AbdimulhinYussuf3675 <abdimulhin.yussuf@student.moringaschool.com>
2021-07-25 16:47:34 +0300

    updated index view

--- home/views.py
+++ home/views.py
@@ -1,7 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http  import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse('Welcome to the Moringa Tribune')
-# Create your views here.
+    if not request.user.is_authenticated:
+        return redirect('signout')
+    else:
+        if request.user.id == 1:
+            if request.method == 'POST':
+                form = NeighborhoodForm(request.POST)
+                if form.is_valid():
+                    neighborhood = Neighborhood(neighborhood_name=request.POST['neighborhood_name'],neighborhood_location=request.POST['neighborhood_location'])
+                    neighborhood.save()
+                return redirect('index')
+            else:
+                form = NeighborhoodForm()
+            neighborhoods = Neighborhood.objects.all()
+            return render(request,'index.html',{'neighborhoods':neighborhoods,'form':form})
+        elif request.user != 1:
+            user = UserProfile.objects.filter(user = request.user).first()
+            if user is None:
+                user = UserProfile(user=request.user)
+                user.save()
+            if user.neighborhood is None:
+                title = 'Neighborhood'
+                neighborhoods = Neighborhood.objects.all()
+                return render(request,'index.html',{'neighborhoods':neighborhoods})
+            else:
+                user = UserProfile.objects.filter(user = request.user).first()
+                return redirect(reverse('neighborhood',args=[user.neighborhood.id]))
+
+def signout(request):
+    logout(request)
+    return redirect('login')
+
+
+
