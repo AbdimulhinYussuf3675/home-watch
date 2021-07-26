98bfbafdf82cb4f7bf588865e2e6817152bf805c
AbdimulhinYussuf3675 <abdimulhin.yussuf@student.moringaschool.com>
2021-07-26 15:25:24 +0300

    added a profileurl

--- home/urls.py
+++ home/urls.py
@@ -6,4 +6,7 @@ urlpatterns=[
     url(r'^signout/$',views.signout,name='signout'),
     url(r'^neighborhood/(\d+)',views.neighborhood,name='neighborhood'),
     url(r'^profile/(\d+)',views.profile,name='profile'),
+    url(r'^add_business/',views.add_business,name='add_business'),
+    url(r'^search/',views.search,name='search'),
+    url(r'^change_neighborhood/(\d+)',views.change_neighborhood,name='change_neighborhood'),
 ]
