4f94c427c94d72c006b7591589066010dd31b2aa
AbdimulhinYussuf3675 <abdimulhin.yussuf@student.moringaschool.com>
2021-07-26 13:42:35 +0300

    added profile view

--- home/views.py
+++ home/views.py
@@ -3,6 +3,7 @@ from django.http  import HttpResponse
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login, logout
 from django.urls import reverse
+from .models import Neighborhood,UserProfile,Business
 from django.contrib.auth.models import User
 import datetime
 
@@ -39,5 +40,48 @@ def signout(request):
     logout(request)
     return redirect('login')
 
+def neighborhood(request,neighborhood_id):
+    if request.user.id == 1:
+        neighborhood = Neighborhood.objects.get(id = neighborhood_id)
+        members = UserProfile.objects.filter(neighborhood = neighborhood).all()
+        emergencies = EmergencyContacts.objects.filter(neighborhood_contact = neighborhood).all()
+        return render(request,'neighborhood.html',{'neighborhood':neighborhood,'members':members,'emergencies':emergencies})
+    else:
+        neighborhood = Neighborhood.objects.get(id = neighborhood_id)
+        user = UserProfile.objects.filter(user = request.user).first()
+        businesses = Business.objects.filter(business_neighborhood=neighborhood).all()
+        emergencies = EmergencyContacts.objects.filter(neighborhood_contact = neighborhood).all()
+        user.neighborhood = neighborhood
+        user.save()
 
+        if request.method == "POST":
+            form = PostForm(request.POST)
+            if form.is_valid():
+                post = Post(title=request.POST['title'],post_description=request.POST['post_description'],posted_by=request.user,post_hood=neighborhood,posted_on=datetime.datetime.now())
+                post.save()
+                return redirect(reverse('neighborhood',args=[neighborhood.id]))
+        else:
+            form = PostForm()
+
+        posts = Post.objects.filter(post_hood = neighborhood).all()
+        return render(request,'neighborhood.html',{'posts':posts,'form':form,'user':user,'businesses':businesses,'neighborhood':neighborhood,'emergencies':emergencies})
+def profile(request,user_id):
+    user = User.objects.get(id = user_id)
+    profile = UserProfile.objects.filter(user = user).first()
+    if request.method == 'POST':
+        form = UpdateProfileForm(request.POST,instance=profile)
+        if form.is_valid():
+            profile.first_name = request.POST['first_name']
+            profile.last_name = request.POST['last_name']
+            profile.location = request.POST['location']
+            profile.save()
+        return redirect(reverse('profile',args=[user.id]))
+    else:
+        form = UpdateProfileForm(instance=profile)
+
+    businesses = Business.objects.filter(owner = user).all()
+    emergencies = EmergencyContacts.objects.filter(neighborhood_contact = profile.neighborhood).all()
+    neighborhoods = Neighborhood.objects.all()
+
+    return render(request,'profile.html',{'neighborhoods':neighborhoods,'businesses':businesses,'profile':profile,'form':form,'emergencies':emergencies})
 
