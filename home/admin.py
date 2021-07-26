5c80d09998df8ef30d26416fb3148ad12f8a397d
AbdimulhinYussuf3675 <abdimulhin.yussuf@student.moringaschool.com>
2021-07-26 13:38:51 +0300

    registered models

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Neighborhood,Business,UserProfile,EmergencyContacts,Post
 
 # Register your models here.
+admin.site.register(Neighborhood)
+admin.site.register(Business)
+admin.site.register(UserProfile)
+admin.site.register(EmergencyContacts)
+admin.site.register(Post)
