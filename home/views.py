b99060dc2f7c042320149e4e2d5327c30be04d91
AbdimulhinYussuf3675 <abdimulhin.yussuf@student.moringaschool.com>
2021-07-26 12:26:27 +0300

    added a datetime

--- home/views.py
+++ home/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render,redirect
 from django.http  import HttpResponse
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login, logout
+from django.urls import reverse
+from django.contrib.auth.models import User
+import datetime
 
 # Create your views here.
 def index(request):
