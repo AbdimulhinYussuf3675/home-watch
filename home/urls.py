aba6c975739df879e3bd6bfc550dbbb7db076df1
AbdimulhinYussuf3675 <abdimulhin.yussuf@student.moringaschool.com>
2021-07-26 13:42:12 +0300

    added profile url

--- home/urls.py
+++ home/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns=[
     url(r'^$',views.index, name='index'),
     url(r'^signout/$',views.signout,name='signout'),
+    url(r'^neighborhood/(\d+)',views.neighborhood,name='neighborhood'),
+    url(r'^profile/(\d+)',views.profile,name='profile'),
 ]
