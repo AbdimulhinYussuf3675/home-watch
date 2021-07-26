1a01b97caa8fc15ab4895e2e38511bb0ba24421d
AbdimulhinYussuf3675 <abdimulhin.yussuf@student.moringaschool.com>
2021-07-26 12:26:53 +0300

    registered bootstrap3

--- hood/settings.py
+++ hood/settings.py
@@ -25,7 +25,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'home'
+    'home',
+    'bootstrap3',
 ]
 
 MIDDLEWARE = [
--- hood/urls.py
+++ hood/urls.py
@@ -1,7 +1,12 @@
 from django.conf.urls import url,include
 from django.contrib import admin
+from django_registration.backends.one_step.views import RegistrationView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'',include('home.urls')),
+    url(r'^accounts/register/',RegistrationView.as_view(success_url='/accounts/login/'),name='django_registration_register'),
+    url(r'^accounts/', include('django_registration.backends.one_step.urls')),
+    url(r'^accounts/', include('django.contrib.auth.urls')),
+    url(r'^tinymce/',include('tinymce.urls'))
 ]
